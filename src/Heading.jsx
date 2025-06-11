54ca65dc9f56526cc8d2bdbde4cfba63c15496ed
neerajkumarsi20 <neerajtnj20@gmail.com>
2025-06-11 22:07:10 +0530

    Initial commit

---
+++ src/Heading.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Heading() {
+  return <h1> My Fav Foods </h1>;
+}
+
+export default Heading;
---
+++ src/List.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li>Apple</li>
+      <li>Orange</li>
+    </ul>
+  );
+}
+
+export default List;
